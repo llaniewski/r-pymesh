f3a45b579bf2b50cbfcba1d3e1a5382bf2f23a9b
L Laniewski-Wollk <uqllanie@uq.edu.au>
2023-09-13 05:38:35 +0000

    spheres from table

--- test.R
+++ test.R
@@ -1,14 +1,22 @@
 library(reticulate)
 use_python("/usr/local/bin/python")
 pymesh = import("pymesh")
-sp = pymesh$generate_icosphere(1, c(0,0,0), 4L)
-mesh = sp
-sp = pymesh$generate_icosphere(1, c(1,0,0), 4L)
-mesh = pymesh$boolean(mesh, sp, "union")
-sp = pymesh$generate_icosphere(1, c(1,0,0), 4L)
-mesh = pymesh$boolean(mesh, sp, "union")
-pymesh$meshio$save_mesh("mesh.stl", mesh, ascii=FALSE)
-mesh$add_attribute("face_area")
-print(sum(mesh$get_attribute("face_area")))
-print(mesh$volume)
+
+calc_sp = function(tab, filename) {
+    for (i in seq_len(nrow(tab))) {
+        sp = pymesh$generate_icosphere(tab$r[i], c(tab$x[i],tab$y[i],tab$z[i]), 4L)
+        if (i == 1) {
+            mesh = sp
+        } else {
+            mesh = pymesh$boolean(mesh, sp, "union")
+        }
+    }
+    if (!missing(filename)) pymesh$meshio$save_mesh(filename, mesh, ascii=FALSE)
+    mesh$add_attribute("face_area")
+    list( area=sum(mesh$get_attribute("face_area")), volume=mesh$volume )
+}
+
+tab = data.frame(r=c(1,1,1),x=c(0,1,0),y=c(0,0,1),z=c(0,0,0))
+calc_sp(tab)
+
 
